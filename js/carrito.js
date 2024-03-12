d7647d39b2dc1a530b690b3a56abb0484400436e
rubng87 <gallegoruben_srye@hotmail.com>
2024-03-12 13:58:11 +0100

    carroCompra

--- js/carrito.js
+++ js/carrito.js
@@ -26,75 +26,39 @@ Recuerda la importancia comentar con detalle el código.
  Lo importante es el cálculo, no los estilos css
  */
 
- // CARRITO APP V1.0.0
- // 2024-03-11
+// CARRITO APP V1.0.0
+// 2024-03-11
 
-//  let listaTareas = []
-//  const pomelo = document.getElementById("pomelo")
-
-// let totalCompra = 0; // Variable para almacenar el precio total de la compra
-
-// function miEleccion(nombreFruta, precio) {  // Con esta función obtenemosel nombre de la fruta y su precio.
-    
-//    let cantidad = prompt(`¿Qué cantidad de ${nombreFruta} deseas:`);
-//    cantidad = Number(cantidad) 
-// //    console.log(typeof cantidad);
-//     let precioTotal = (precio * cantidad).toFixed(2); 
-//     // console.log(`Compra : ${nombreFruta} -> ${precioTotal}`);
-
-
-
-
-
-    // Variable global para almacenar la suma de los productos
-// let sumaTotal = 0;
-
-//   function miEleccion(nombreFruta, precio) {  
-//     let cantidad = prompt(`¿Qué cantidad de ${nombreFruta} deseas:`);
-//     cantidad = Number(cantidad); // convierto el  string a numero
-    
-
-//     let precioTotal = (precio * cantidad).toFixed(2);
-//     // console.log(precioTotal);
-//     sumaTotal += parseFloat(precioTotal); // Sumamos el precio total al total acumulado
-//     console.log(sumaTotal);
-    
-//     document.getElementById("preuFinal").innerText = sumaTotal.toFixed(2) + "€"; // Actualizamos el HTML con el total
-// }
-
-
-// function listaTotalCompra() {
-//   let totalPrecio = 0;
-//   let listaCompra = documment.getElementById("preuFinal");
-//   listaCompra.innerText = ""
-
-//   if (parseInt(document.getElementById("pomelo").value) > 0) {
-//     let cantidad1 = parseInt(document.getElementById("cantidad1").value);
-//     let listaCompra1 = document.createElement("li");
-//     listaCompra1.textContent = "Producto 1 - Cantidad: " + cantidad1 + " - Total: $" + cantidad1 * 10;
-//    console.log(pomelo);
-//   }
-// }
-
-
-let sumaTotal = 0;
+let sumaTotal = 0; // Variable para almacenar el precio total de la compra
 let listaFrutas = []; // Lista para almacenar las cantidades y precios de las frutas seleccionadas
 
-let carrito = document.getElementById("carrito")
-let preuFinal = document.getElementById("preuFinal")
+let carrito = document.getElementById("carrito") // Creamos la variable "carrito" y le decimos donde esta en el HTML
+let preuFinal = document.getElementById("preuFinal") // Creamos la variable "preuFinal" y le decimos donde esta en el HTML
 
-function miEleccion(nombreFruta, precio, unidad) {  
-    let cantidad = prompt(`¿Qué cantidad de ${nombreFruta} deseas:`);
-    cantidad = Number(cantidad); // convierto el  string a numero
-    
-    let precioTotal = (precio * cantidad).toFixed(2);
-    sumaTotal += parseFloat(precioTotal); // Sumamos el precio total al total acumulado
-    
+function miEleccion(nombreFruta, precio, unidad) {  // Con esta función obtenemos el nombre de la fruta, precio y la unidad de las frutas seleccionadas, Que se la hemos asignado manualmente en el HTML
+    let cantidad = prompt(`¿Qué cantidad de ${nombreFruta} deseas:`); // Hacemos un prompt para preguntar las cantidades  le añadimos ${nombreFruta}para que nos diga el nombre difernte de cada fruta.
+    cantidad = Number(cantidad); // Pasamos el string a numero porque nos lo daba como String y necesitamos que el tipo sea numero para a continuación poder hacer las operaciones
+
+    let precioTotal = (precio * cantidad).toFixed(2); //Creamos la variable para almacenar el precio total de la compra, y añadimos un ".toFixed(2)" para que nos diga el precio con 2 decimales.
+    sumaTotal += parseFloat(precioTotal); // hacemos la suma de los precios de las frutas seleccionadas
+
     // Añadir la información de la fruta seleccionada a la lista
     listaFrutas.push({ fruta: nombreFruta, cantidad: cantidad, precioTotal: parseFloat(precioTotal) });
 
-    carrito.innerHTML += `<p>${nombreFruta} ${cantidad} ${unidad} x ${precio} €/${unidad} = ${precioTotal}€</p>`; // Actualizamos el HTML con la lista de producto
+    carrito.innerHTML += `<p><i class="fas fa-trash-alt fa-sm" style="color:red" onclick="borrarProducto(${listaFrutas.length - 1})"></i> ${nombreFruta} ${cantidad} ${unidad} x ${precio} €/${unidad} = ${precioTotal} €</p>`; // le añadimos una "p" y le asignamos todo lo que queremos que aparezca en la lista que se va acumulando de compra como, "nombreFruta, cantidad, precio, unidad y al final el resultado."
     preuFinal.innerText = sumaTotal.toFixed(2) + "€"; // Actualizamos el HTML con el total
+}
+
+
+
+function borrarProducto(indice) {
+  // Restar el precio del producto borrado al total
+  sumaTotal -= listaFrutas[indice].precioTotal; //Resta el precio del producto que se está eliminando al total acumulado (sumaTotal). listaFrutas[index] accede al objeto del producto en la lista listaFrutas en la posición index, y precioTotal es todo lo acumulado.
+  preuFinal.innerText = sumaTotal.toFixed(2) + "€"; // Actualizamo el total despues de quitarlo anteriormente.
+
+  
+  listaFrutas.splice(indice, 1);// Eliminar el producto de la lista y del carrito, usamos el splice() para eliminar un elemento del array listaFrutas en la posición index y el número "1" indica que solo se eliminará un elemento en esa posición.
+  carrito.removeChild(carrito.childNodes[indice]); //Elimina el elemento del carrito de la pantalla. Y el "carrito.childNodes[index]" accede al nodo hijo del carrito en la posición index, y removeChild() lo elimina.
 }
 
 
@@ -106,13 +70,4 @@ function miEleccion(nombreFruta, precio, unidad) {
 
 
 
-
-
-
-
-
-
-
-
-   
 
